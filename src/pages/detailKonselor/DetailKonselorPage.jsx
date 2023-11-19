30612b67e87ad8e754c032d3c7504dba20c1b63b
Fresinsya <vicafresinsya@gmail.com>
2023-11-19 13:45:31 +0700

    title, card

---
+++ src/pages/detailKonselor/DetailKonselorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './Title'
+import CardKonselor from './CardKonselor'
+
+const DetailKonselorPage = () => {
+    return (
+        <>
+            <Title />
+            <CardKonselor />
+        </>
+    )
+}
+
+export default DetailKonselorPage
--- src/router/RouterManagement.jsx
+++ src/router/RouterManagement.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/landingPage/LandingPage";
+import DetailKonselorPage from "../pages/detailKonselor/DetailKonselorPage";
 
 function RouterManagement() {
   return (
@@ -8,6 +9,7 @@ function RouterManagement() {
       <Suspense>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/detailKonselor" element={<DetailKonselorPage />} />
         </Routes>
       </Suspense>
     </div>
