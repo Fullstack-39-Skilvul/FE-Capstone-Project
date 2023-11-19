8b6bc46f3e7cac8eccac5ff3c4772fab2683932b
Fresinsya <vicafresinsya@gmail.com>
2023-11-19 13:06:26 +0700

    responsive

--- src/pages/konselorPage/Title.jsx
+++ src/pages/konselorPage/Title.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Title = () => {
     return (
         <>
-            <header className='mt-14 mb-6'>
-                <div className='flex gap-2 justify-center w-full'>
-                    <h1 className='font-bold text-[#0F2650] text-4xl'>
+            <header className='mt-14 mb-6 sm:text-center max-sm:text-center'>
+                <div className='flex gap-2 justify-center w-full '>
+                    <h1 className='font-bold text-[#0F2650] text-4xl max-sm:text-3xl'>
                         Profesional
                     </h1>
-                    <h1 className='font-bold text-[#1C79F2] text-4xl'>
+                    <h1 className='font-bold text-[#1C79F2] text-4xl max-sm:text-3xl'>
                         Konselor
                     </h1>
                 </div>
