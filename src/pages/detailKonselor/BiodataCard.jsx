e77bcb7cc7cfcefc098b47d1d7bb810231fa7e71
Fresinsya <vicafresinsya@gmail.com>
2023-11-19 21:01:17 +0700

    datepicker, jamkonseling

--- src/pages/detailKonselor/BiodataCard.jsx
+++ src/pages/detailKonselor/BiodataCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Spesifikasi from './Spesifikasi'
 import { Link } from 'react-router-dom'
 import JamKonseling from './JamKonseling'
+import DateKonselor from './DateKonselor'
 
 const BiodataCard = () => {
     return (
@@ -12,9 +13,13 @@ const BiodataCard = () => {
                         Dr. Amanda Wijaya, seorang psikolog klinis berpengalaman, telah memberikan kontribusi yang tak terhitung dalam membantu individu menavigasi perjalanan kesehatan mental mereka. Dengan pendekatan terapeutik yang holistik dan kecerdasan emosional yang tinggi, Amanda tidak hanya memahami tantangan yang dihadapi kliennya tetapi juga berkomitmen untuk membimbing mereka menuju pemulihan yang berkelanjutan. Ia menjalankan praktiknya dengan penuh kehangatan dan empati, menciptakan ruang yang aman bagi klien untuk tumbuh dan berkembang. Dengan dedikasi terhadap penelitian dan pendidikan, Amanda bukan hanya seorang praktisi, tetapi juga seorang pemimpin dalam memajukan bidang psikologi klinis.
                     </p>
                     <p className='flex justify-center rounded-3xl bg-[#ffdb38cd] font-bold text-md px-5 py-2'>
-                    "Setiap langkah kecil adalah kemajuan menuju keberhasilan, jangan pernah meremehkan kekuatan potensial yang tersimpan dalam upaya konsisten dan tekad tak kenal menyerah."
+                        "Setiap langkah kecil adalah kemajuan menuju keberhasilan, jangan pernah meremehkan kekuatan potensial yang tersimpan dalam upaya konsisten dan tekad tak kenal menyerah."
                     </p>
-                    <JamKonseling />
+                    <div className='flex justify-center w-full gap-3'>
+                        <JamKonseling />
+                        <DateKonselor />
+                    </div>
+
                     <button className='flex justify-end w-full mt-6'>
                         <Link className='flex p-2 px-4 bg-[#063D82] text-white text-sm rounded-2xl' to={'/'}>Booking Now</Link>
                     </button>
--- src/pages/detailKonselor/JamKonseling.jsx
+++ src/pages/detailKonselor/JamKonseling.jsx
@@ -15,20 +15,20 @@ const JamKonseling = () => {
     };
     return (
         <>
-            <div className="inline-flex bg-white border rounded-md">
+            <div className="inline-flex mt-4 bg-white border border-[#1C79F2] rounded-2xl">
                 <button
                     type="button"
                     onClick={toggleDropdown}
-                    className="px-4 py-2 text-sm text-gray-600 hover:text-gray-700 hover:bg-gray-50 rounded-l-md"
+                    className="px-4 py-2 w-44 text-sm text-gray-600 hover:text-gray-700 hover:bg-gray-50 rounded-l-2xl"
                 >
                     {selectedTime || 'Waktu Konseling'}
                 </button>
 
-                <div className="relative">
+                <div className="relative ">
                     <button
                         type="button"
                         onClick={toggleDropdown}
-                        className="inline-flex items-center justify-center h-full px-2 text-gray-600 border-l border-gray-100 hover:text-gray-700 rounded-r-md hover:bg-gray-50"
+                        className="inline-flex items-center justify-center h-full px-2 text-gray-600 border-l border-gray-100 hover:text-gray-700 rounded-r-2xl hover:bg-gray-50"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +43,7 @@ const JamKonseling = () => {
                     </button>
 
                     {isOpen && (
-                        <div className="absolute right-0 z-10 w-56 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
+                        <div className="absolute right-0 z-10 w-40 mt-4 origin-top-right bg-white border border-gray-100 rounded-md shadow-lg">
                             <div className="flex flex-wrap p-2 justify-center">
                                 <button
                                     type="button"
