dd9e893e7f89db871eb2db97bf87bf5787db3102
Fresinsya <vicafresinsya@gmail.com>
2023-11-19 12:44:59 +0700

    done

--- src/pages/konselorPage/KonselorPage.jsx
+++ src/pages/konselorPage/KonselorPage.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
 
 import CardKonselor from './CardKonselor'
+import Title from './Title'
 
 const KonselorPage = () => {
     return (
         <>
-            <header className='mt-14 mb-6'>
-                <div className='flex gap-2 justify-center w-full'>
-                    <h1 className='font-bold text-[#0F2650] text-4xl'>
-                        Profesional
-                    </h1>
-                    <h1 className='font-bold text-[#1C79F2] text-4xl'>
-                        Konselor
-                    </h1>
-                </div>
-                <p className='flex justify-center w-full'>
-                    Temukan Paket Konseling yang Tepat untuk Mendukung Kesehatan Mental dan Kesejahteraan Anda
-                </p>
-            </header>
+            <Title />
             <div className='flex flex-wrap justify-center'>
                 <CardKonselor />
                 <CardKonselor />
