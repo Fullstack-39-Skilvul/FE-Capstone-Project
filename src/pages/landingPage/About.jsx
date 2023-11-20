8101898f2d3c8657a60ee8aef39bdb69aad04500
vikiade00 <vikiade00@gmail.com>
2023-11-20 15:45:42 +0700

    Add about section

---
+++ src/pages/landingPage/About.jsx
@@ -0,0 +1,45 @@
+import { Brain } from "phosphor-react";
+import React from "react";
+import { DATA_ABOUT } from "./constant";
+
+function About() {
+  return (
+    <div className=" min-h-[100vh] mx-10 md:mx-32">
+      <div className="flex flex-col mt-16 justify-center items-center md:text-center">
+        <div className="text-2xl md:text-4xl text-center font-bold text-blue-950">
+          Mental Sehat Untuk <br />
+          <span className="text-sky-500">Indonesia</span>
+        </div>
+        <p className="text-stone-600 mt-5 text-justify">
+          Selamat datang di Mahasucita, platform konseling kesehatan mental yang
+          memahami betapa pentingnya menjaga kesejahteraan mental dalam
+          kehidupan modern kita. Kami berkomitmen untuk membantu Anda meraih
+          keseimbangan dalam hidup, mengatasi tantangan mental, dan mencapai
+          potensi penuh Anda. Di sini, kami ingin berbagi dengan Anda mengenai
+          apa yang membuat Mahasucita unik :
+        </p>
+      </div>
+      <div className=" mt-16">
+        <div className="flex flex-wrap justify-between items-center gap-4 md:gap-0">
+          {DATA_ABOUT.map((item) => (
+            <div
+              key={item.id}
+              className="flex items-center justify-center flex-col border max-w-[320px] min-h-[300px] p-7 rounded-[40px] border-blue-950"
+            >
+              <div className=" flex justify-center items-center gap-1 text-md md:text-xl mb-5 mt-1 text-blue-950 font-semibold">
+                {item.icon}
+                {item.judul1}
+                <span className="text-sky-500">{item.judul2}</span>
+              </div>
+              <p className="text-center text-stone-600 text-sm md:text-base">
+                {item.deskripsi}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/landingPage/constant.jsx
@@ -0,0 +1,28 @@
+import { Brain, HeartBreak, UsersFour } from "phosphor-react";
+
+export const DATA_ABOUT = [
+  {
+    id: 1,
+    icon: <Brain />,
+    judul1: "Kesehatan",
+    judul2: "Mental",
+    deskripsi:
+      "Kami memahami bahwa kesehatan mental adalah aset berharga yang harus dijaga dengan serius. Kami telah menyediakan platform yang aman, mendukung, dan bersahabat untuk Anda menjalani proses konseling dan perawatan.",
+  },
+  {
+    id: 2,
+    icon: <UsersFour />,
+    judul1: "Team",
+    judul2: "Profesional",
+    deskripsi:
+      "Kami bekerja sama dengan tim profesional kesehatan mental berlisensi yang memiliki pengalaman luas dalam berbagai bidang seperti psikologi, psikiatri, dan konseling. Mereka siap membantu Anda mengatasi berbagai masalah mental.",
+  },
+  {
+    id: 3,
+    icon: <HeartBreak />,
+    judul1: "Kenyamanan",
+    judul2: "Pengguna",
+    deskripsi:
+      "Kami memahami bahwa waktu dan jarak bisa menjadi hambatan dalam mencari bantuan. Oleh karena itu, kami menyediakan layanan konseling online yang nyaman, sehingga Anda dapat mengakses bantuan kapan saja dan di mana saja.",
+  },
+];
