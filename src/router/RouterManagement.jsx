e00d494fcde836d96d5f3faa3ce3bce34bc1faf1
azalia-virna <azaliavirna@gmail.com>
2023-11-26 06:52:19 +0700

    Menambahkan contact page

--- src/router/RouterManagement.jsx
+++ src/router/RouterManagement.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPageLayout from "./layout/LandingPageLayout";
 import AdminPageLayout from "./layout/AdminPageLayout";
+import ContactPage from "../pages/contactPage/ContactPage";
 
 function RouterManagement() {
   return (
@@ -9,6 +10,7 @@ function RouterManagement() {
       <Suspense>
         <Routes>
           <Route path="/" element={<LandingPageLayout />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="/admin/*" element={<AdminPageLayout />} />
         </Routes>
       </Suspense>
