051d4e6f8d16feea0ebd14bc1597b2bb0d9fe29c
vikiade00 <vikiade00@gmail.com>
2023-11-27 12:53:48 +0700

    fix conflic

--- src/router/layout/LandingPageLayout.jsx
+++ src/router/layout/LandingPageLayout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../../pages/landingPage/LandingPage";
-import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
 
 function LandingPageLayout() {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
