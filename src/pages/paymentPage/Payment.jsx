50859d5364245009d0e3cc42c789ab4e777d8186
Azalia Virna <azaliavirna@gmail.com>
2023-12-01 07:35:42 +0700

    revisi membuat data payment

--- src/pages/paymentPage/Payment.jsx
+++ src/pages/paymentPage/Payment.jsx
@@ -6,15 +6,25 @@ import { useState } from "react";
 function Payment() {
   const [selectedBank, setSelectedBank] = useState('');
   const handlePaymentProofSubmit = async () => {
-    try {
-      const response = await axios.post('https://be-capstone-project.vercel.app/payments', {
-        selectedBank, 
-      });
-      console.log('Response from API:', response.data);
-    } catch (error) {
-      console.error('Error sending payment', error);
-    }
-  };
+  try {
+    const response = await axios.post(
+      'https://be-capstone-project.vercel.app/payments',
+      {
+        selectedBank,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjQxMjUwNzI3YjE0MWQ0M2NlNWM4MyIsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzAxMDY2NzMxfQ.d9ADnKK-sYhF1HvlfzF8mVdGfQPR9xb987m707OD-zM',
+        },
+      }
+    );
+    console.log('Response from API:', response.data);
+  } catch (error) {
+    console.error('Error sending payment', error);
+  }
+};
+
   return (
     <div className="flex flex-col  justify-center items-center">
       <div className="text-2xl lg:mt-[100px] md:mb-5 md:text-4xl text-center font-bold text-blue-950">
